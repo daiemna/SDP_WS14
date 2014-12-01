d19685c8474c062cc41d8b5f79aa7c9acfb3f422
deebuls <deebuls@gmail.com>
2014-12-01 01:05:31 +0100

    Test File Not compiling

---
+++ cvb/tests/cameraTest.cpp
@@ -0,0 +1,17 @@
+#include "cameraTest.h"
+
+//Tests that the LoadImageFile works
+//
+TEST_F(iCVCUtilitiesTest, LoadingImage) {
+
+    static const size_t DRIVERPATHSIZE = 256;
+
+    // load the camera
+    char driverPath[DRIVERPATHSIZE] = { 0 };
+    TranslateFileName("%CVB%/drivers/GenICam.vin", driverPath, DRIVERPATHSIZE);
+    IMG hCamera = NULL;
+    
+    EXPECT_PRED2(LoadImageFile, driverPath, hCamera);    
+    
+    EXPECT_TRUE(LoadImageFile, driverPath, hCamera);
+}
